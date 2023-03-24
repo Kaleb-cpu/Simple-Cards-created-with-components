9e3e569afce622ccbd43510fd6244ca4d940ed34
Kaleb-cpu <kakuj424@gmail.com>
2023-03-23 18:26:57 -0600

    card style added

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -3,21 +3,40 @@
 </script>
 
 <main>
-<div class="card">
+<div class="class">
   <a href="{card.cardLink}">
-  <figure>
-    <h1>{card.name}</h1>
-    <img src="{card.image}" width="300" height="380" alt="red">
-    <figcaption>{card.description}</figcaption>
-  </figure>
-</a>
+    <figure>
+      <h1>{card.name}</h1>
+      <img src="{card.image}" width="200" height="280" alt="red">
+      <figcaption>{card.description}</figcaption>
+    </figure>
+  </a>
 </div>  
 </main>
 
 <style> 
-  main{
+  main {
     flex: auto;
   }
+  .class {
+    border: 5px solid darkcyan;
+    width: max-content;
+    margin: auto;
+    border-radius: 1em;
+    text-align: center;
+  }
+  .class:hover {
+    background-color: beige;
 
-
+  }
+  
+  .class a{
+    text-decoration: none;
+    color: black;
+  }
+  
+  figcaption{
+    width: 30ch;
+    font-size: 1.2rem;
+  }
 </style>
